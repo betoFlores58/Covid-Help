6e1f9851c0036eb539fe86e4d5916038e6b07be0
Arte <arte.solo.arte@hotmail.com>
2021-05-13 11:21:30 -0500

    Creación de rama 'Foro' y de layout y clase'

---
+++ Covid-Help/app/src/main/java/com/covid/help/ForoActivity.kt
@@ -0,0 +1,11 @@
+package com.covid.help
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_foro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foro)
+    }
+}
